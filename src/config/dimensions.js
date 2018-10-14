5f3198b09172d0db69773156de6eb7526dabe792
Mike Lothar <mikelothar@gmail.com>
2018-10-14 15:04:49 +0200

    go full

---
+++ src/config/dimensions.js
@@ -0,0 +1,4 @@
+export const canvas = {
+  width: window.screen.width,
+  height: window.screen.height,
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import 'phaser';
 
+import {version} from '../package.json'
 import {SimpleScene} from './scenes/simple-scene';
+import { canvas } from './config/dimensions'
 
 const gameConfig = {
-  width: 680,
-  height: 400,
+  title: 'Misimi the Sequal',
+  url: 'http://misimi2.netlify.com',
+  version,
+  banner: {
+    hidePhaser: true
+  },
+  type: Phaser.AUTO,
+  height: canvas.height,
+  width: canvas.width,
+  // transparent: true,
   scene: SimpleScene
 };
 
